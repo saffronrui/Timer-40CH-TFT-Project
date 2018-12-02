3e40a9ed5fd240badd370bad0292955067fce0c9
saffronrui <liuruiruijob@126.com>
2018-12-02 17:35:48 +0800

    Timer-40CH-TFT-Project
    1. This is project for 7-inch touch screen For Guangzhou Dacai screen
    2. 40-Input signal
    3. expeiment done, work well.

---
+++ output/DefineID.h
@@ -0,0 +1,296 @@
+//创建画面Screen0的命令索引ID
+#define  _SCREEN_SCREEN0                                                       0
+
+//创建画面Screen1的命令索引ID
+#define  _SCREEN_SCREEN1                                                       1
+
+//画面Screen0的背景图片
+#define  _IMG_SCREEN0                                                          0
+
+#define  _RTC_SCREEN0_RTC1                                                     4
+
+#define  _PROGRESS_SCREEN0_PROGRESS14                                          5
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY11                                     60
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY25                                     61
+
+#define  _BTN_SCREEN0_BUTTON1                                                  1
+
+#define  _BTN_SCREEN0_BUTTON2                                                  7
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY1                                      62
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY2                                      63
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY3                                      64
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY4                                      65
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY5                                      66
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY6                                      67
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY7                                      68
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY8                                      69
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY9                                      70
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY10                                     71
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY12                                     72
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY13                                     73
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY14                                     74
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY15                                     75
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY16                                     76
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY17                                     77
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY18                                     78
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY19                                     79
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY22                                     81
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY23                                     82
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY24                                     83
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY26                                     84
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY27                                     85
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY28                                     86
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY29                                     87
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY30                                     88
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY31                                     89
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY32                                     90
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY33                                     91
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY34                                     92
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY35                                     93
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY36                                     94
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY37                                     95
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY38                                     96
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY39                                     97
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY40                                     98
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY41                                     99
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY42                                    100
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY43                                    101
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY44                                    102
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY45                                    103
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY46                                      8
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY47                                      2
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY48                                      6
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY49                                      9
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY50                                     10
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY51                                     11
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY52                                     12
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY53                                     13
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY54                                     14
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY55                                     15
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY56                                     16
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY57                                     17
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY58                                     18
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY59                                     19
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY60                                     20
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY61                                     21
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY62                                     22
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY63                                     23
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY64                                     24
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY65                                     25
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY66                                     26
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY67                                     27
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY68                                     28
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY69                                     29
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY20                                     80
+
+//画面Screen1的背景图片
+#define  _IMG_SCREEN1                                                          1
+
+#define  _PROGRESS_SCREEN1_PROGRESS1                                          15
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY11                                     60
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY25                                     61
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY1                                      62
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY2                                      63
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY3                                      64
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY4                                      65
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY5                                      66
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY6                                      67
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY7                                      68
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY8                                      69
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY9                                      70
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY10                                     71
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY12                                     72
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY13                                     73
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY14                                     74
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY15                                     75
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY16                                     76
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY17                                     77
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY18                                     78
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY19                                     79
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY20                                     80
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY21                                     81
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY22                                     82
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY23                                     83
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY24                                     84
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY26                                     85
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY27                                     86
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY28                                     87
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY29                                     88
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY30                                     89
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY31                                     90
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY32                                     91
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY33                                     92
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY34                                     93
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY35                                     94
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY36                                     95
+
+#define  _RTC_SCREEN1_RTC1                                                    47
+
+#define  _BTN_SCREEN1_BUTTON1                                                  1
+
+#define  _BTN_SCREEN1_BUTTON2                                                  7
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY46                                      8
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY47                                      2
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY48                                      6
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY49                                      9
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY50                                     10
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY51                                     11
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY52                                     12
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY37                                      3
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY38                                      4
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY39                                      5
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY40                                     13
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY41                                     14
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY42                                     16
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY43                                     17
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY44                                     18
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY45                                     19
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY53                                     20
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY54                                     21
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY55                                     22
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY56                                     23
+
+#define  _BTN_SCREEN1_BUTTON3                                                 27
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY57                                     25
+
+#define  _PROGRESS_SCREEN1_PROGRESS2                                          26
+
+#define  _BTN_SCREEN1_BUTTON4                                                 28
+
+#define  _BTN_SCREEN1_BUTTON5                                                 30
+
+#define  _BTN_SCREEN1_BUTTON6                                                 32
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY58                                     29
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY59                                     31
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY60                                     33
+
+#define  _TXT_DIS__SCREEN1_TEXT_DISPLAY61                                     34
+
